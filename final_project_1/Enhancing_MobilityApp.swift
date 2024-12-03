30847f9f7fbdb203fffed3d39516d4427bfd1b3b
brenden20 <118419078+brenden20@users.noreply.github.com>
2024-12-03 13:47:59 -0600

    Project Demo Files

---
+++ final_project_1/Enhancing_MobilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enhancing_MobilityApp.swift
+//  Enhancing Mobility
+//
+//  Created by Brenden DeLuna on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Enhancing_MobilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+}
